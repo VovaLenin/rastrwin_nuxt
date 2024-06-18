63447d7365cc8cf309c0f1835f1901576c141a2a
VovaLenin <ustinov2708@gmail.com>
2024-06-18 03:48:54 +0300

    починил составление матрицы проводимостей

--- utils/createMatrixY.js
+++ utils/createMatrixY.js
@@ -17,14 +17,14 @@ function createMatrixY(isConnectivity, values) {
     // ВВОД ПРОВОДИМОСТЕЙ УЗЛОВ
     for (let i = 0; i < nodes; i++) {
       const numder = Number(values.nodes[i][0]);
-      const Yd = Number(values.nodes[i][7]);
-      const Ym = Number(values.nodes[i][8]);
+      const Yd = Number(values.nodes[i][7]) * 0.000001;
+      const Ym = Number(values.nodes[i][8]) * 0.000001;
       Y[numder - 1][numder - 1] = Y[numder - 1][numder - 1].add(
         new Complex(Yd, Ym)
       );
     }
-    console.log(Y);
-    //   ВВОД ПРОВОДИМОСТЕЙ ВЕТВЕЙ
+
+    // ВВОД ПРОВОДИМОСТЕЙ ВЕТВЕЙ
     for (let i = 0; i < values.branches.length; i++) {
       const NN = Number(values.branches[i][1]);
       const NK = Number(values.branches[i][2]);
@@ -33,11 +33,14 @@ function createMatrixY(isConnectivity, values) {
       const K = Number(values.branches[i][5]);
       const Z = new Complex(R, X);
       const KT = new Complex(K, 0);
-      Y[NN - 1][NN - 1] = Y[NN - 1][NN - 1].add(Z.inverse());
-      Y[NK - 1][NK - 1] = Y[NK - 1][NK - 1].add(Z.inverse().mul(KT.mul(KT)));
-      Y[NN - 1][NK - 1] = Y[NN - 1][NK - 1].sub(Z.inverse().mul(KT));
-      Y[NK - 1][NN - 1] = Y[NK - 1][NN - 1].sub(Z.inverse().mul(KT));
+
+      const inverseZ = Z.inverse();
+      Y[NN - 1][NN - 1] = Y[NN - 1][NN - 1].add(inverseZ);
+      Y[NK - 1][NK - 1] = Y[NK - 1][NK - 1].add(inverseZ.mul(KT.mul(KT)));
+      Y[NN - 1][NK - 1] = Y[NN - 1][NK - 1].sub(inverseZ.mul(KT));
+      Y[NK - 1][NN - 1] = Y[NK - 1][NN - 1].sub(inverseZ.mul(KT));
     }
+
     const Y1 = roundValues(Y, 3);
     console.log("МАТРИЦА ПРОВОДИМОСТИ", Y1);
     return Y1;
